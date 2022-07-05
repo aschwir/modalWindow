0263b6c9b07e3af3817790b70fccb96f20b3ed9e
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-04 21:15:14 -0400

    added eventlisteners and DOM selectors

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+console.log(btnOpenModal);
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", function () {
+    console.log("Button Clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
