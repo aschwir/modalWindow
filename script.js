05b93001bff6d17632b27ad7a9898a21b8a31645
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-04 21:39:57 -0400

    refactored and added more functionality

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnOpenModal);
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal(){
+        modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener("click", function () {
-    console.log("Button Clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+    btnOpenModal[i].addEventListener("click", openModal)
 }
+
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
