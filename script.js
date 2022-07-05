3e34870f419a12aab6b88bbbc061afad48bad8d7
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-05 08:31:40 -0400

    refactored

--- script.js
+++ script.js
@@ -10,15 +10,20 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
-const openModal(){
-        modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-}
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-    btnOpenModal[i].addEventListener("click", openModal)
+  btnOpenModal[i].addEventListener("click", openModal);
 }
-
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
